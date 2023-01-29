2e73094822f133471faa94933a6671e986040cfd
ChustaD <chusta2013@mail.ru>
2023-01-29 14:17:23 +0300

    Added deploy

--- vite.config.js
+++ vite.config.js
@@ -5,14 +5,24 @@ import vue from "@vitejs/plugin-vue";
 import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-    VitePWA({ registerType: "autoUpdate", devOptions: { enabled: false } }),
-  ],
-  resolve: {
-    alias: {
-      "@": fileURLToPath(new URL("./src", import.meta.url)),
+export default defineConfig(({ command, mode, ssrBuild }) => {
+  const config = {
+    base: "/ToDoDay/",
+    plugins: [
+      vue(),
+      VitePWA({ registerType: "autoUpdate", devOptions: { enabled: false } }),
+    ],
+    resolve: {
+      alias: {
+        "@": fileURLToPath(new URL("./src", import.meta.url)),
+      },
     },
-  },
+  };
+  if (command === "serve") {
+    config.base = "/";
+    return config;
+  } else {
+    // command === 'build'
+    return config;
+  }
 });
